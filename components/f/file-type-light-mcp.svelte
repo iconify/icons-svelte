d5c8e87acd3df8575bdb1e9035c774f388c7aa32
Vjacheslav Trushkin <cyberalien@gmail.com>
2026-09-20 13:41:19 +0300

    chore: update files

--- components/f/file-type-light-mcp.svelte
+++ components/f/file-type-light-mcp.svelte
@@ -5,16 +5,16 @@ import Icon from '@iconify/css-svelte';
 let {width, height, ...props} = $props();
 
 const viewBox = {"width":32,"height":32};
-const content = `<path class="wfcjskb5t"/>`;
+const content = `<path class="uy5li62sa"/>`;
 </script>
 <Icon width={width} height={height} viewBox={viewBox} content={content} fallback="vscode-icons:file-type-light-mcp" {...props}></Icon>
 <style>
-:global(.wfcjskb5t) {
+:global(.uy5li62sa) {
   fill: none;
   stroke: var(--svg-color--000, #000);
   stroke-linecap: round;
   stroke-width: var(--svg-stroke-width--1-7px, 1.7px);
-  d: path("m4.4 15l11-11c1.5-1.5 4-1.5 5.5 0v0c1.5 1.5 1.5 4 0 5.5l-8.3 8.3m0 0l8.3-8.3c1.5-1.5 4-1.5 5.5 0l.057.057c1.5 1.5 1.5 4 0 5.5l-10 10c-.51.51-.51 1.3 0 1.8l2 2m-.4-22l-8.1 8.1c-1.5 1.5-1.5 4 0 5.5v0c1.5 1.5 4 1.5 5.5 0l8.1-8.1");
+  d: path("m4.4 15l11-11c1.5-1.5 4-1.5 5.5 0s1.5 4 0 5.5l-8.3 8.3m0 0l8.3-8.3c1.5-1.5 4-1.5 5.5 0l.057.057c1.5 1.5 1.5 4 0 5.5l-10 10c-.51.51-.51 1.3 0 1.8l2 2m-.4-22l-8.1 8.1c-1.5 1.5-1.5 4 0 5.5s4 1.5 5.5 0l8.1-8.1");
 }
 
 </style>
