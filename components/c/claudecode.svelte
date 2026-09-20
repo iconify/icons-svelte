1aff6a38ce74018dd0cf8906b3a72b126ca6b867
Vjacheslav Trushkin <cyberalien@gmail.com>
2026-09-20 13:26:16 +0300

    chore: update files

---
+++ components/c/claudecode.svelte
@@ -0,0 +1,11 @@
+<script>
+import Icon from '@iconify/css-svelte';
+import '../../css/a/au73r9tay.css';
+
+/** @type {{width?: string; height?: string;}} */
+let {width, height, ...props} = $props();
+
+const viewBox = {"width":128,"height":128};
+const content = `<path clip-rule="evenodd" class="au73r9tay"/>`;
+</script>
+<Icon width={width} height={height} viewBox={viewBox} content={content} fallback="devicon:claudecode" {...props}></Icon>
