bc4ae04b72cbc9207480ec0c9947ade02cd75793
Vjacheslav Trushkin <cyberalien@gmail.com>
2026-09-20 13:25:34 +0300

    chore: update files

--- components/e/elixir-wordmark.svelte
+++ components/e/elixir-wordmark.svelte
@@ -1,14 +1,11 @@
 <script>
 import Icon from '@iconify/css-svelte';
-import '../../css/s/sbpq65m9a.css';
-import '../../css/k/k_0btnw4n.css';
-import '../../css/u/ukkseuxmb.css';
-import '../../css/u/u-93mqbgq.css';
+import '../../css/f/fozui9hfo.css';
 
 /** @type {{width?: string; height?: string;}} */
 let {width, height, ...props} = $props();
 
 const viewBox = {"width":128,"height":128};
-const content = `<path class="sbpq65m9a"/><use href="#SVGD0oThevo"/><path class="k_0btnw4n"/><use x="23.9" href="#SVGD0oThevo"/><path class="ukkseuxmb"/><defs><path id="SVGD0oThevo" class="u-93mqbgq"/></defs>`;
+const content = `<path class="fozui9hfo"/>`;
 </script>
 <Icon width={width} height={height} viewBox={viewBox} content={content} fallback="devicon-plain:elixir-wordmark" {...props}></Icon>
--- components/e/elixir.svelte
+++ components/e/elixir.svelte
@@ -1,14 +1,11 @@
 <script>
 import Icon from '@iconify/css-svelte';
-import '../../css/y/y1j4pw88e.css';
-import '../../css/v/vchls_btl.css';
-import '../../css/e/ejfm2-94j.css';
-import '../../css/t/t9egpnbaz.css';
+import '../../css/l/lweui9bhw.css';
 
 /** @type {{width?: string; height?: string;}} */
 let {width, height, ...props} = $props();
 
 const viewBox = {"width":128,"height":128};
-const content = `<path class="y1j4pw88e"/><path class="vchls_btl"/><path class="ejfm2-94j"/><path class="t9egpnbaz"/>`;
+const content = `<path class="lweui9bhw"/>`;
 </script>
 <Icon width={width} height={height} viewBox={viewBox} content={content} fallback="devicon-plain:elixir" {...props}></Icon>
