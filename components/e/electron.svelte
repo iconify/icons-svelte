c4451481709b722d13619a985d1d83dd5cd03546
Vjacheslav Trushkin <cyberalien@gmail.com>
2026-09-20 13:39:57 +0300

    chore: update files

--- components/e/electron.svelte
+++ components/e/electron.svelte
@@ -4,7 +4,7 @@ import '../../css/k/k6qizcvhk.css';
 import '../../css/y/yk9ufrxqu.css';
 import '../../css/y/ych83_bej.css';
 import '../../css/n/n1mrteb8e.css';
-import '../../css/q/q-3p_7bja.css';
+import '../../css/a/a8cw4obis.css';
 import '../../css/i/ixq1o8b5k.css';
 import '../../css/w/wm3v2rb1w.css';
 
@@ -12,6 +12,6 @@ import '../../css/w/wm3v2rb1w.css';
 let {width, height, ...props} = $props();
 
 const viewBox = {"width":128,"height":128};
-const content = `<circle class="k6qizcvhk"/><path class="yk9ufrxqu"/><path class="ych83_bej"/><path class="n1mrteb8e"/><path clip-rule="evenodd" class="q-3p_7bja"/><ellipse class="ixq1o8b5k"/><path clip-rule="evenodd" class="wm3v2rb1w"/>`;
+const content = `<circle class="k6qizcvhk"/><path class="yk9ufrxqu"/><path class="ych83_bej"/><path class="n1mrteb8e"/><path clip-rule="evenodd" class="a8cw4obis"/><ellipse class="ixq1o8b5k"/><path clip-rule="evenodd" class="wm3v2rb1w"/>`;
 </script>
 <Icon width={width} height={height} viewBox={viewBox} content={content} fallback="thesvg-color:electron" {...props}></Icon>
