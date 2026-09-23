9ec92af3016cba92aa99873bac368a9e328559d6
Vjacheslav Trushkin <cyberalien@gmail.com>
2026-09-23 16:20:45 +0300

    chore: update files

---
+++ components/n/nodebo.svelte
@@ -0,0 +1,13 @@
+<script>
+import Icon from '@iconify/css-svelte';
+import '../../css/j/ja77f2b5e.css';
+import '../../css/s/sb-7x_b3r.css';
+import '../../css/f/fzmbfkb6j.css';
+
+/** @type {{width?: string; height?: string;}} */
+let {width, height, ...props} = $props();
+
+const viewBox = {"width":120,"height":120};
+const content = `<g class="ja77f2b5e"><circle class="sb-7x_b3r"/><path class="fzmbfkb6j"/></g>`;
+</script>
+<Icon width={width} height={height} viewBox={viewBox} content={content} fallback="thesvg:nodebo" {...props}></Icon>
--- components/o/okta.svelte
+++ components/o/okta.svelte
@@ -1,11 +1,11 @@
 <script>
 import Icon from '@iconify/css-svelte';
-import '../../css/b/b07s0zb2q.css';
+import '../../css/o/oqm9wbc2m.css';
 
 /** @type {{width?: string; height?: string;}} */
 let {width, height, ...props} = $props();
 
-const viewBox = {"width":24,"height":24};
-const content = `<path class="b07s0zb2q"/>`;
+const viewBox = {"width":33,"height":33};
+const content = `<path clip-rule="evenodd" class="oqm9wbc2m"/>`;
 </script>
 <Icon width={width} height={height} viewBox={viewBox} content={content} fallback="thesvg:okta" {...props}></Icon>
