495ca3173856d9daafa3894399cb0536e0260f52
Vjacheslav Trushkin <cyberalien@gmail.com>
2026-09-26 14:54:30 +0300

    chore: update files

---
+++ components/p/phone-top-right-with-exclamation-point.svelte
@@ -0,0 +1,11 @@
+<script>
+import Icon from '@iconify/css-svelte';
+import '../../css/a/ag3oe707j.css';
+
+/** @type {{width?: string; height?: string;}} */
+let {width, height, ...props} = $props();
+
+const viewBox = {"width":15,"height":15};
+const content = `<path class="ag3oe707j"/>`;
+</script>
+<Icon width={width} height={height} viewBox={viewBox} content={content} fallback="pinhead:phone-top-right-with-exclamation-point" {...props}></Icon>
