fd4981a96a4ddab0cfc32ab47f1686907ce26862
Vjacheslav Trushkin <cyberalien@gmail.com>
2026-09-26 14:51:44 +0300

    chore: update files

---
+++ components/h/house-cog.svelte
@@ -0,0 +1,14 @@
+<script>
+import Icon from '@iconify/css-svelte';
+import '../../css/n/nrj6p8qat.css';
+import '../../css/b/budom8kqj.css';
+import '../../css/a/ae-aa10mx.css';
+import '../../css/e/ez1x61b2a.css';
+
+/** @type {{width?: string; height?: string;}} */
+let {width, height, ...props} = $props();
+
+const viewBox = {"width":24,"height":24};
+const content = `<g class="nrj6p8qat"><path class="budom8kqj"/><path class="ae-aa10mx"/><circle class="ez1x61b2a"/></g>`;
+</script>
+<Icon width={width} height={height} viewBox={viewBox} content={content} fallback="lucide:house-cog" {...props}></Icon>
---
+++ components/l/letters.svelte
@@ -0,0 +1,11 @@
+<script>
+import Icon from '@iconify/css-svelte';
+import '../../css/z/zp4ubxbuo.css';
+
+/** @type {{width?: string; height?: string;}} */
+let {width, height, ...props} = $props();
+
+const viewBox = {"width":24,"height":24};
+const content = `<path class="zp4ubxbuo"/>`;
+</script>
+<Icon width={width} height={height} viewBox={viewBox} content={content} fallback="lucide:letters" {...props}></Icon>
