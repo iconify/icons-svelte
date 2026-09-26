37f01a5edfba9b92475319fdf43408f482ea6047
Vjacheslav Trushkin <cyberalien@gmail.com>
2026-09-26 14:59:45 +0300

    chore: update files

---
+++ components/f/file-type-quarto.svelte
@@ -0,0 +1,18 @@
+<script>
+import Icon from '@iconify/css-svelte';
+
+/** @type {{width?: string; height?: string;}} */
+let {width, height, ...props} = $props();
+
+const viewBox = {"width":32,"height":32};
+const content = `<path clip-rule="evenodd" class="y8xs-xg-t"/>`;
+</script>
+<Icon width={width} height={height} viewBox={viewBox} content={content} fallback="vscode-icons:file-type-quarto" {...props}></Icon>
+<style>
+:global(.y8xs-xg-t) {
+  fill: var(--svg-color--74aadb, #74aadb);
+  fill-rule: evenodd;
+  d: path("M15 15.24V2C7.96 2.5 2.38 8.2 2 15.24Zm1.76 0H30A14.05 14.05 0 0 0 16.76 2zM15 17H2.02a14 14 0 0 0 12.96 12.95V17zm1.76 0v13a14 14 0 0 0 13.22-12.98H16.76z");
+}
+
+</style>
