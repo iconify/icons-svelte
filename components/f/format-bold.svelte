d2b7ed6fdc52e0630998fc904d1c056c6e9ec033
Vjacheslav Trushkin <cyberalien@gmail.com>
2026-09-26 15:00:35 +0300

    chore: update files

--- components/f/format-bold.svelte
+++ components/f/format-bold.svelte
@@ -1,11 +1,13 @@
 <script>
 import Icon from '@iconify/css-svelte';
-import '../../css/l/l4mil5bye.css';
+import '../../css/j/jx0p4fbya.css';
+import '../../css/d/d12fgu-ud.css';
+import '../../css/e/ercpunbkt.css';
 
 /** @type {{width?: string; height?: string;}} */
 let {width, height, ...props} = $props();
 
 const viewBox = {"width":24,"height":24};
-const content = `<path vector-effect="non-scaling-stroke" class="l4mil5bye"/>`;
+const content = `<g class="jx0p4fbya"><path vector-effect="non-scaling-stroke" class="d12fgu-ud"/><path vector-effect="non-scaling-stroke" class="ercpunbkt"/></g>`;
 </script>
 <Icon width={width} height={height} viewBox={viewBox} content={content} fallback="wordpress:format-bold" {...props}></Icon>
---
+++ components/j/justify-space-around.svelte
@@ -0,0 +1,13 @@
+<script>
+import Icon from '@iconify/css-svelte';
+import '../../css/j/jx0p4fbya.css';
+import '../../css/q/qdasuqiss.css';
+import '../../css/l/lsg8msbeo.css';
+
+/** @type {{width?: string; height?: string;}} */
+let {width, height, ...props} = $props();
+
+const viewBox = {"width":24,"height":24};
+const content = `<g class="jx0p4fbya"><path class="qdasuqiss"/><path vector-effect="non-scaling-stroke" class="lsg8msbeo"/></g>`;
+</script>
+<Icon width={width} height={height} viewBox={viewBox} content={content} fallback="wordpress:justify-space-around" {...props}></Icon>
